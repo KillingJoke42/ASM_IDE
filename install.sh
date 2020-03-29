cee64cc6c8de1ecb5b8d4ee1a3d007c978e4a847
KillingJoke42 <49367372+KillingJoke42@users.noreply.github.com>
2020-03-29 18:14:27 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python3
+sudo apt-get install python3-pip
+python3 -m pip install yaml
+python3 -m pip install json
+python3 -m pip install numpy
+python3 -m pip install sklearn
+python3 -m pip install pyqt5
+python3 -m pip install pyaudio
+python3 -m pip install SpeechRecognition
